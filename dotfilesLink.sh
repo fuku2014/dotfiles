d430b7d71738ebfcca014f9436a543a7b3ceca05
fuku <fukuzawa.makoto@nifty.co.jp>
2015-08-03 00:39:14 +0000

    fix shell

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -3,3 +3,4 @@ binary_path=`cd \`dirname $0\`; pwd`
 ln -s ${binary_path}/.vimrc ~/.vimrc
 ln -s ${binary_path}/.gitconfig ~/.gitconfig
 ln -s ${binary_path}/.tmux.conf ~/.tmux.conf
+ln -s ${binary_path}/.vim ~/.vim
